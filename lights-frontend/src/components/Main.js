1ca445fb313cb22b95076cc90bf0d5260b5308b8
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2020-12-27 11:57:34 -0600

    connected some endpoints

--- lights-frontend/src/components/Main.js
+++ lights-frontend/src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Button, Container, TextField } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
@@ -8,15 +8,71 @@ import Input from '@material-ui/core/Input';
 import { ColorPicker } from 'material-ui-color';
 
 
-function clear(e) {
-    e.preventDefault()
-    console.log("Function Called!");
-    fetch('/basics/clear', {
-        method: 'GET',
-    }).then(alert("Complete"))
-}
 
 export default function Main(props) {
+
+    const [pixel, setPixel] = useState(0);
+    const [colorOne, setColorOne] = useState(null);
+    const [colorTwo, setColorTwo] = useState([0, 0, 0]);
+    const [colorThree, setColorThree] = useState([0, 0, 0]);
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(0);
+
+    function handleColorOneChange(color) {
+        setColorOne(color);
+    };
+
+    function handlePixelIndexChange(e) {
+        e.preventDefault()
+        setPixel(e.target.value);
+    }
+
+    function handleStartIndexChange(e) {
+        e.preventDefault()
+        setStartIndex(e.target.value);
+    }
+
+    function handleEndIndexChange(e) {
+        e.preventDefault()
+        setEndIndex(e.target.value);
+    }
+
+    function clear(e) {
+        e.preventDefault()
+        console.log("Function Called!");
+        fetch('/basics/clear', {
+            method: 'GET',
+        }).then(alert("Complete"))
+    }
+
+    function setPixelColor(e) {
+        e.preventDefault()
+        console.log("Function Called");
+        fetch('/basics/set_pixel', {
+            method: 'GET',
+            body: JSON.stringify({'color': colorOne.rgb, 'index': pixel}),
+        }).then(alert("Complete"));
+    }
+
+    function fillRange(e) {
+        e.preventDefault()
+        console.log("Function Called");
+        fetch('/basics/fill_range', {
+            method: 'GET',
+            body: JSON.stringify({'color': colorOne.rgb, 'start': startIndex, 'end': endIndex}),
+        }).then(alert("Complete"));
+    }
+
+    function fill(e) {
+        e.preventDefault()
+        console.log("Function Called");
+        fetch('/basics/fill', {
+            method: 'GET',
+            body: JSON.stringify({'color': colorOne.rgb}),
+        }).then(alert("Complete"));
+    }
+
+
     return (
         <div className="main">
             <Grid container spacing={4}>
@@ -32,16 +88,17 @@ export default function Main(props) {
                     <Typography align="left" variant="h5">Clears the current leds. Setting them all to off.</Typography>
                 </Grid>
 
+
                 <Grid item xs={6}>
-                    <form style={{flexDirection: 'row', display: 'flex'}}>
+                    <form onSubmit={setPixelColor} style={{flexDirection: 'row', display: 'flex'}}>
                         <Container style={{width: "40%"}}>
-                            <Button variant="contained" color="primary" fullWidth>Fill Section</Button>
+                            <Button type="submit" variant="contained" color="primary" fullWidth>Fill Section</Button>
                         </Container>
                         <Container style={{width: "20%"}}>
-                            <ColorPicker defaultValue="red" hideTextfield></ColorPicker>
+                            <ColorPicker onChange={handleColorOneChange} value={colorOne} name="color" hideTextfield></ColorPicker>
                         </Container>
                         <Container style={{width: "20%"}}>
-                            <TextField type="number" defaultValue="0"></TextField>
+                            <TextField onChange={handlePixelIndexChange} value={pixel} type="number"></TextField>
                         </Container>
                         <Container style={{width: "20%"}}>
                         </Container>
@@ -51,19 +108,20 @@ export default function Main(props) {
                     <Typography align="left" variant="h5">Sets the color of a given led to a specified color.</Typography>
                 </Grid>
 
+
                 <Grid item xs={6}>
-                    <form style={{flexDirection: 'row', display: 'flex'}}>
+                    <form onSubmit={fillRange} style={{flexDirection: 'row', display: 'flex'}}>
                         <Container style={{width: "40%"}}>
-                            <Button variant="contained" color="primary" fullWidth>Fill Section</Button>
+                            <Button type="sumbit" variant="contained" color="primary" fullWidth>Fill Section</Button>
                         </Container>
                         <Container style={{width: "20%"}}>
-                            <ColorPicker defaultValue="red" hideTextfield></ColorPicker>
+                            <ColorPicker onChange={handleColorOneChange} value={colorOne} name="color" hideTextfield></ColorPicker>
                         </Container>
                         <Container style={{width: "20%"}}>
-                            <TextField type="number" defaultValue="0"></TextField>
+                            <TextField onChange={handleStartIndexChange} value={pixel} type="number"></TextField>
                         </Container>
                         <Container style={{width: "20%"}}>
-                            <TextField type="number" defaultValue="0"></TextField>
+                            <TextField onChange={handleEndIndexChange} value={pixel} type="number"></TextField>
                         </Container>
                     </form>
                 </Grid>
@@ -71,10 +129,11 @@ export default function Main(props) {
                     <Typography align="left" variant="h5">Fills specified range of leds to the specified color.</Typography>
                 </Grid>
 
+
                 <Grid item xs={6}>
-                    <form style={{flexDirection: 'row', display: 'flex'}}>
+                    <form onSubmit={fill} style={{flexDirection: 'row', display: 'flex'}}>
                         <Container style={{width: "40%"}}>
-                            <Button variant="contained" color="primary" fullWidth>Fill All</Button>
+                            <Button type="submit" variant="contained" color="primary" fullWidth>Fill All</Button>
                         </Container>
                         <Container style={{width: "60%"}}>
                             <ColorPicker defaultValue="red" hideTextfield></ColorPicker>
@@ -91,7 +150,7 @@ export default function Main(props) {
                             <Button variant="contained" color="primary" fullWidth>Fill All</Button>
                         </Container>
                         <Container style={{width: "60%"}}>
-                            <ColorPicker defaultValue="red" hideTextfield></ColorPicker>
+                            <ColorPicker onChange={handleColorOneChange} value={colorOne} name="color" hideTextfield></ColorPicker>
                         </Container>
                     </form>
                 </Grid>

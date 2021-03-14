0d5025086c50bcb3d6a46d39a778046ec0582c6c
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2021-03-14 18:06:59 -0400

    new button layouts

--- lights-frontend/src/components/Basics.js
+++ lights-frontend/src/components/Basics.js
@@ -11,17 +11,33 @@ export default function Basics(props) {
     const BASE_URL = "http://127.0.0.1:5000"
 
     const [colorOne, setColorOne] = useState(null);
+    const [colorTwo, setColorTwo] = useState(null);
+    const [colorThree, setColorThree] = useState(null);
     const [startIndex, setStartIndex] = useState(0);
+    const [pixelIndex, setPixelIndex] = useState(0);
     const [endIndex, setEndIndex] = useState(0);
 
     function handleColorOneChange(color) {
         setColorOne(color);
     };
 
+    function handleColorTwoChange(color) {
+        setColorTwo(color);
+    };
+
+    function handleColorThreeChange(color) {
+        setColorThree(color);
+    };
+
 
     function handleStartIndexChange(e) {
         e.preventDefault()
         setStartIndex(e.target.value);
+    }
+
+    function handlePixelIndexChange(e) {
+        e.preventDefault()
+        setPixelIndex(e.target.value);
     }
 
     function handleEndIndexChange(e) {
@@ -44,7 +60,7 @@ export default function Basics(props) {
         else {
             fetch(BASE_URL + '/basics/set_pixel', {
                 method: 'POST',
-                body: JSON.stringify({'color': colorOne.rgb, 'index': startIndex}),
+                body: JSON.stringify({'color': colorOne.rgb, 'index': pixelIndex}),
             })
         }
     }
@@ -57,7 +73,7 @@ export default function Basics(props) {
         else {
             fetch(BASE_URL + '/basics/fill_range', {
                 method: 'POST',
-                body: JSON.stringify({'color': colorOne.rgb, 'start': startIndex, 'end': endIndex}),
+                body: JSON.stringify({'color': colorTwo.rgb, 'start': startIndex, 'end': endIndex}),
             })
         }
     }
@@ -70,7 +86,7 @@ export default function Basics(props) {
         else {
             fetch(BASE_URL + '/basics/fill', {
                 method: 'POST',
-                body: JSON.stringify({'color': colorOne.rgb}),
+                body: JSON.stringify({'color': colorThree.rgb}),
             })
         }
     }
@@ -79,41 +95,64 @@ export default function Basics(props) {
     return (
         <div className="main">
             <Grid container spacing={3} className="patterns">
-                <Grid item xs={2}></Grid>
-                <Grid item xs={8}>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={setPixelColor} variant="contained" color="primary">Set Pixel</Button>
+                </Grid>
+                <Grid item xs={2}>
                     <InputLabel>Pick a color</InputLabel>
                     <ColorPicker onChange={handleColorOneChange} value={colorOne} name="color" hideTextfield></ColorPicker>
                 </Grid>
+                <Grid item xs={2}>
+                    <Container>
+                        <InputLabel>Pixel Index</InputLabel>
+                        <TextField onChange={handlePixelIndexChange} value={pixelIndex} type="number"></TextField>
+                    </Container>
+                </Grid>
                 <Grid item xs={2}></Grid>
-                <Container>
-                    <InputLabel>Start/Pixel Index</InputLabel>
-                    <TextField onChange={handleStartIndexChange} value={startIndex} type="number"></TextField>
-                </Container>
-                <Container style={{marginTop: "2%"}}>
-                    <InputLabel>End Index</InputLabel>
-                    <TextField onChange={handleEndIndexChange} value={endIndex} type="number"></TextField>
-                </Container>
-
-                <Grid item xs={12}>
+                <Grid item xs={4}>
                     <Button onClick={clear} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Clear</Button>
                 </Grid>
 
+                {/* Fill Section */}
 
-
-                <Grid item xs={4}>
-                    <Container style={{width: "40%"}}>
-                        <Button className="patternButton" onClick={setPixelColor} variant="contained" color="primary">Set Pixel</Button>
-                    </Container>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={fillRange} variant="contained" color="primary">Fill Section</Button>
                 </Grid>
-                <Grid item xs={4}>
-                    <Container style={{width: "40%"}}>
-                        <Button className="patternButton" onClick={fillRange} variant="contained" color="primary">Fill Section</Button>
-                    </Container>
+                <Grid item xs={2}>
+                    <InputLabel>Pick a color</InputLabel>
+                    <ColorPicker onChange={handleColorTwoChange} value={colorTwo} name="color" hideTextfield></ColorPicker>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item xs={2}>
                     <Container>
-                        <Button className="patternButton" onClick={fill} variant="contained" color="primary" >Fill All</Button>
+                        <InputLabel>Start Index</InputLabel>
+                        <TextField onChange={handleStartIndexChange} value={startIndex} type="number"></TextField>
                     </Container>
+                </Grid>
+                <Grid item xs={2}>
+                    <Container style={{marginTop: "2%"}}>
+                        <InputLabel>End Index</InputLabel>
+                        <TextField onChange={handleEndIndexChange} value={endIndex} type="number"></TextField>
+                    </Container>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button onClick={clear} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Clear</Button>
+                </Grid>
+
+                {/* Fill All */}
+
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={fill} variant="contained" color="primary" >Fill All</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <InputLabel>Pick a color</InputLabel>
+                    <ColorPicker onChange={handleColorThreeChange} value={colorThree} name="color" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={4}>
+                    <Button onClick={clear} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Clear</Button>
                 </Grid>
             </Grid>
         </div>
--- lights-frontend/src/components/Patterns.js
+++ lights-frontend/src/components/Patterns.js
@@ -13,6 +13,12 @@ export default function Patterns(props) {
     const [colorOne, setColorOne] = useState(null);
     const [colorTwo, setColorTwo] = useState(null);
     const [colorThree, setColorThree] = useState(null);
+    const [colorFour, setColorFour] = useState(null);
+    const [colorFive, setColorFive] = useState(null);
+    const [colorSix, setColorSix] = useState(null);
+    const [colorSeven, setColorSeven] = useState(null);
+    const [colorEight, setColorEight] = useState(null);
+    const [colorNine, setColorNine] = useState(null);
 
     function handleStop(e) {
         e.preventDefault()
@@ -36,117 +42,117 @@ export default function Patterns(props) {
 
     function setTriAlternate(e) {
         e.preventDefault()
-        if (colorOne == null || colorTwo == null || colorThree == null) {
+        if (colorTwo == null || colorThree == null || colorFour == null) {
             alert("You must enter three colors");
         }
         else {
             fetch(BASE_URL + '/patterns/set_tri_alternating', {
                 method: 'POST',
-                body: JSON.stringify({'color_one': colorOne.rgb, 'color_two': colorTwo.rgb, 'color_three': colorThree.rgb}),
+                body: JSON.stringify({'color_one': colorTwo.rgb, 'color_two': colorThree.rgb, 'color_three': colorFour.rgb}),
             })
         }
     }
 
     function setAlternate(e) {
         e.preventDefault()
-        if (colorOne == null || colorTwo == null) {
+        if (colorFive == null || colorSix == null) {
             alert("You must enter two colors");
         }
         else {
             fetch(BASE_URL + '/patterns/set_alternating', {
                 method: 'POST',
-                body: JSON.stringify({'color_one': colorOne.rgb, 'color_two': colorTwo.rgb}),
+                body: JSON.stringify({'color_one': colorFive.rgb, 'color_two': colorSix.rgb}),
             })
         }
     }
 
     function startAlternating(e) {
         e.preventDefault()
-        if (colorOne == null || colorTwo == null) {
+        if (colorFive == null || colorSix == null) {
             alert("You must enter two colors");
         }
         else {
             fetch(BASE_URL + '/patterns/start_alternating', {
                 method: 'POST',
-                body: JSON.stringify({'color_one': colorOne.rgb, 'color_two': colorTwo.rgb}),
+                body: JSON.stringify({'color_one': colorFive.rgb, 'color_two': colorSix.rgb}),
             })
         }
     }
 
     function startTriAlternating(e) {
         e.preventDefault()
-        if (colorOne == null || colorTwo == null || colorThree == null) {
+        if (colorTwo == null || colorThree == null || colorFour == null) {
             alert("You must enter three colors");
         }
         else {
             fetch(BASE_URL + '/patterns/start_tri_alternating', {
                 method: 'POST',
-                body: JSON.stringify({'color_one': colorOne.rgb, 'color_two': colorTwo.rgb, 'color_three': colorThree.rgb}),
+                body: JSON.stringify({'color_one': colorTwo.rgb, 'color_two': colorThree.rgb, 'color_three': colorFour.rgb}),
             })
         }
     }
 
     function shootLeft(e) {
         e.preventDefault()
-        if (colorOne == null) {
+        if (colorSeven == null) {
             alert("You must enter a color");
         }
         else {
             fetch(BASE_URL + '/patterns/shoot_left', {
                 method: 'POST',
-                body: JSON.stringify({'color': colorOne.rgb}),
+                body: JSON.stringify({'color': colorSeven.rgb}),
             })
         }
     }
 
     function slideLeft(e) {
         e.preventDefault()
-        if (colorOne == null) {
+        if (colorSeven == null) {
             alert("You must enter a color");
         }
         else {
             fetch(BASE_URL + '/patterns/slide_left', {
                 method: 'POST',
-                body: JSON.stringify({'color': colorOne.rgb}),
+                body: JSON.stringify({'color': colorSeven.rgb}),
             })
         }
     }
 
     function slideMiddle(e) {
         e.preventDefault()
-        if (colorOne == null) {
+        if (colorSeven == null) {
             alert("You must enter a color");
         }
         else {
             fetch(BASE_URL + '/patterns/slide_middle', {
                 method: 'POST',
-                body: JSON.stringify({'color': colorOne.rgb}),
+                body: JSON.stringify({'color': colorSeven.rgb}),
             })
         }
     }
 
     function slideRight(e) {
         e.preventDefault()
-        if (colorOne == null) {
+        if (colorSeven == null) {
             alert("You must enter a color");
         }
         else {
             fetch(BASE_URL + '/patterns/slide_right', {
                 method: 'POST',
-                body: JSON.stringify({'color': colorOne.rgb}),
+                body: JSON.stringify({'color': colorSeven.rgb}),
             })
         }
     }
 
     function shootRight(e) {
         e.preventDefault()
-        if (colorOne == null) {
+        if (colorSeven == null) {
             alert("You must enter a color");
         }
         else {
             fetch(BASE_URL + '/patterns/shoot_right', {
                 method: 'POST',
-                body: JSON.stringify({'color': colorOne.rgb}),
+                body: JSON.stringify({'color': colorSeven.rgb}),
             })
         }
     }
@@ -155,64 +161,125 @@ export default function Patterns(props) {
     return (
         <div className="patterns">
             <Grid container className="patterns" spacing={3}>
-                <Grid item xs={3}></Grid>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={flashFade} variant="contained" color="primary">Flash Fade</Button>
+                </Grid>
                 <Grid item xs={2}>
                     <InputLabel>Color One</InputLabel>
                     <ColorPicker onChange={color => setColorOne(color)} value={colorOne} name="colorOne" hideTextfield></ColorPicker>
                 </Grid>
                 <Grid item xs={2}>
-                    <InputLabel>Color Two</InputLabel>
-                    <ColorPicker onChange={color => setColorTwo(color)} value={colorTwo} name="colorTwo" hideTextfield></ColorPicker>
                 </Grid>
                 <Grid item xs={2}>
-                    <InputLabel>Color Three</InputLabel>
-                    <ColorPicker onChange={color => setColorThree(color)} value={colorThree} name="colorThree" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button onClick={handleStop} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Stop Playing</Button>   
+                </Grid>
+
+                {/* Set Tri Alternate */}
+
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={setTriAlternate} variant="contained" color="primary">Set Tri Alternate</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button className="patternButton" onClick={startTriAlternating} variant="contained" color="primary">Start Tri Alternating</Button>
+                </Grid>
+                <Grid item xs={1}>
+                    <InputLabel>Color 1</InputLabel>
+                    <ColorPicker onChange={color => setColorTwo(color)} value={colorTwo} name="colorOne" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid item xs={1}>
+                    <InputLabel>Color 2</InputLabel>
+                    <ColorPicker onChange={color => setColorThree(color)} value={colorThree} name="colorTwo" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid item xs={1}>
+                    <InputLabel>Color 3</InputLabel>
+                    <ColorPicker onChange={color => setColorFour(color)} value={colorFour} name="colorThree" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={4}>
+                    <Button onClick={handleStop} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Stop Playing</Button>   
+                </Grid>
+
+                {/* Set Alternate */}
+
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={setAlternate} variant="contained" color="primary">Set Alternate</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button className="patternButton" onClick={startAlternating} variant="contained" color="primary">Start Alternating</Button>
+                </Grid>
+                <Grid item xs={1}>
+                    <InputLabel>Color 1</InputLabel>
+                    <ColorPicker onChange={color => setColorFive(color)} value={colorFive} name="colorOne" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid item xs={1}>
+                    <InputLabel>Color 2</InputLabel>
+                    <ColorPicker onChange={color => setColorSix(color)} value={colorSix} name="colorTwo" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid itex xs={2}></Grid>
+                <Grid item xs={4}>
+                    <Button onClick={handleStop} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Stop Playing</Button>   
+                </Grid>
+
+                {/* Shoot/Slide Left Shoot/Slide Right*/}
+
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={shootLeft} variant="contained" color="primary">Shoot Left</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button className="patternButton" onClick={slideLeft} variant="contained" color="primary">Slide Left</Button>
+                </Grid>
+                <Grid item xs={1}>
+                    <InputLabel>Color 1</InputLabel>
+                    <ColorPicker onChange={color => setColorSeven(color)} value={colorSeven} name="colorOne" hideTextfield></ColorPicker>
                 </Grid>
                 <Grid item xs={3}></Grid>
+                <Grid item xs={4}>
+                    <Button onClick={handleStop} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Stop Playing</Button>   
+                </Grid>
 
-                <Grid item xs={12}>
+
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={slideMiddle} variant="contained" color="primary">Slide Middle</Button>
+                </Grid>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={1}>
+                    <InputLabel>Color 1</InputLabel>
+                    <ColorPicker onChange={color => setColorSeven(color)} value={colorSeven} name="colorOne" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid xs={3}></Grid>
+                <Grid item xs={4}>
+                    <Button onClick={handleStop} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Stop Playing</Button>   
+                </Grid>
+
+
+                <Grid item xs={1}></Grid>
+                <Grid item xs={1}>
+                    <Button className="patternButton" onClick={slideRight} variant="contained" color="primary">Slide Right</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button className="patternButton" onClick={shootRight} variant="contained" color="primary">Shoot Right</Button>
+                </Grid>
+                <Grid item xs={1}>
+                    <InputLabel>Color 1</InputLabel>
+                    <ColorPicker onChange={color => setColorSeven(color)} value={colorSeven} name="colorOne" hideTextfield></ColorPicker>
+                </Grid>
+                <Grid itex xs={3}></Grid>
+                <Grid item xs={4}>
                     <Button onClick={handleStop} variant="contained" style={{backgroundColor: 'red', marginLeft: '20%', marginRight: '20%', width: '50%'}}>Stop Playing</Button>   
                 </Grid>
 
 
 
-                <Grid item xs={1}></Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={flashFade} variant="contained" color="primary" fullWidth>Flash Fade</Button>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={setTriAlternate} variant="contained" color="primary" fullWidth>Set Tri Alternate</Button>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={setAlternate} variant="contained" color="primary" fullWidth>Set Alternate</Button>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={startAlternating} variant="contained" color="primary" fullWidth>Start Alternating</Button>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={startTriAlternating} variant="contained" color="primary" fullWidth>Start Tri Alternating</Button>
-                </Grid>
-                <Grid item xs={1}></Grid>
 
 
 
-
-                <Grid item xs={1}></Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={shootLeft} variant="contained" color="primary" fullWidth>Shoot Left</Button>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={slideLeft} variant="contained" color="primary" fullWidth>Slide Left</Button>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={slideMiddle} variant="contained" color="primary" fullWidth>Slide Middle</Button>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={slideRight} variant="contained" color="primary" fullWidth>Slide Right</Button>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button className="patternButton" onClick={shootRight} variant="contained" color="primary" fullWidth>Shoot Right</Button>
-                </Grid>
                 <Grid item xs={1}></Grid>
             </Grid>
         </div>
